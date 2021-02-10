2f2a6387dd88c873f9b0c565b2e0dc6932444028
ilyaTheCreator <respondatanytime@gmail.com>
2021-02-10 20:39:38 +0300

    messed up

--- client/src/App.js
+++ client/src/App.js
@@ -9,35 +9,7 @@ import useStyles from './styles';
 import memories from './images/memories.png';
 
 const App = () => {
-    // id of a selected memory
-    const [currentId, setCurrentId] = useState(null)
-    const dispatch = useDispatch();
-    const classes = useStyles();
-
-    useEffect(() => {
-        dispatch(getPosts());
-    }, [dispatch]);
-
-    return (
-        <Container maxWidth="lg">
-            <AppBar className={classes.appBar} position="static" color="inherit">
-                <Typography className={classes.heading} variant="h2" align="center">Ananas!!!</Typography>
-                <img className={classes.image} src={memories} alt="icon" height="60" />
-            </AppBar>
-            <Grow in>
-                <Container>
-                    <Grid container justify="space-between" alignItems="stretch" spacing={3}>
-                        <Grid item xs={12} sm={7}>
-                            <Posts setCurrentId={setCurrentId} />
-                        </Grid>
-                        <Grid item xs={12} sm={4}>
-                            <Form currentId={currentId} setCurrentId={setCurrentId} />
-                        </Grid>
-                    </Grid>
-                </Container>
-            </Grow>
-        </Container>
-    );
+    return <p>hello</p>
 };
 
 export default App;
